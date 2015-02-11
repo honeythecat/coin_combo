a47c62e8158b812913c5816610f87c1368e6fa8e
Heather Anderson & Frank Meza <student@epicodus.com>
2015-02-11 13:48:09 -0800

    Add functionality for nickels and pennies.

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,24 @@ var coinCombo = function(change) {
 
 
     var quarters = Math.floor(change / 0.25);
-    totalCoins.push((quarters.toString()).concat(" quarters"));
+    totalCoins.push((quarters.toString()).concat(" quarter(s)"));
 
-    var dimes = Math.floor((change - (quarters * 0.25)).toPrecision(2) / 0.1);
-    totalCoins.push((dimes.toString()).concat(" dimes"));
+    var dimes = Math.floor((change - (quarters * 0.25)).toPrecision(2) / 0.10);
+    totalCoins.push((dimes.toString()).concat(" dime(s)"));
+
+    var nickels = Math.floor((change - (quarters * 0.25) - (dimes * 0.1)).toPrecision(2) / 0.05);
+    totalCoins.push((nickels.toString()).concat(" nickel(s)"));
+
+    var pennies = Math.floor((change - (quarters * 0.25) - (dimes * 0.1) - (nickels * 0.05)).toPrecision(2) / 0.01);
+    totalCoins.push((pennies.toString()).concat(" penny(ies)"));
 
     return totalCoins;
-
 };
 
 
 
 
-//   if (year % 4 === 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-//
+
 // $(document).ready(function() {
 //   $("form#leap-year").submit(function(event) {
 //     var year = parseInt($("input#year").val());
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,18 @@
 describe ("coinCombo", function() {
   it("gives the greatest number of quarters", function() {
-    expect(coinCombo(0.25)).to.eql(["1 quarters", "0 dimes"]);
+    expect(coinCombo(0.25)).to.eql(["1 quarter(s)", "0 dime(s)", "0 nickel(s)", "0 penny(ies)"]);
   });
 
   it("gives the greatest number of quarters then dimes", function() {
-    expect(coinCombo(0.95)).to.eql(["3 quarters", "2 dimes"]);
+    expect(coinCombo(0.2)).to.eql(["0 quarter(s)", "2 dime(s)", "0 nickel(s)", "0 penny(ies)"]);
+  });
+
+  it("gives the greatest number of quarters, dimes, nickels", function() {
+    expect(coinCombo(0.90)).to.eql(["3 quarter(s)", "1 dime(s)", "1 nickel(s)", "0 penny(ies)"]);
+  });
+
+  it("gives the greatest number of quarters, dimes, nickels, and pennies", function() {
+    expect(coinCombo(0.99)).to.eql(["3 quarter(s)", "2 dime(s)", "0 nickel(s)", "4 penny(ies)"]);
   });
 
 });
